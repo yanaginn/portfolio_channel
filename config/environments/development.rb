ebae8e103c76dd3ccf61507c81ce0368244f7436
yanaginn <9663nagi@gmail.com>
2022-04-03 15:19:11 +0000

    add : ユーザログイン機能追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "001a425998e7463c89b21544c1adbd7d.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
