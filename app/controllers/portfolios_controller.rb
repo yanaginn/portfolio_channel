4303929702b8f0a82aa38eb025954e63382958c4
yanaginn <9663nagi@gmail.com>
2022-04-08 05:39:31 +0000

    add : ポートフォリオ詳細画面編集

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,10 +3,20 @@ class PortfoliosController < ApplicationController
   end
 
   def show
+    @portfolio = Portfolio.find(params[:id])
   end
 
   def new
     @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = current_user.portfolios.build(portfolio_params)
+    if @portfolio.save
+      redirect_to portfolio_path(@portfolio), notice: "ポートフォリオを投稿しました。"
+    else
+      render :new
+    end
   end
 
   def edit
